a1395a6fd55e0cdc08e8b44162fbd19a9140c4cc
Justin Rastelli <Jusman98@gmail.com>
2013-11-21 06:57:46 -0800

    Moving the map to its own special class

---
+++ src/com/jcrastelli/wikinearme/MapActivity.java
@@ -0,0 +1,145 @@
+package com.jcrastelli.wikinearme;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapActivity extends FragmentActivity implements
+ConnectionCallbacks,
+OnConnectionFailedListener,
+LocationListener,
+OnMyLocationButtonClickListener{
+	//private ArrayList<String> items = new ArrayList<String>();
+	//private static final LatLng BURIEN = new LatLng(47.6097, -122.3331);
+	
+	// These settings are the same as the settings for the map. They will in fact give you updates
+    // at the maximal rates currently possible.
+    private static final LocationRequest REQUEST = LocationRequest.create()
+            .setInterval(5000)         // 5 seconds
+            .setFastestInterval(16)    // 16ms = 60fps
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+	public GoogleMap theMap;
+	public LocationClient mLocationClient;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //setUpMapIfNeeded();
+        //setUpLocationClientIfNeeded();
+        mLocationClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mLocationClient != null) {
+            mLocationClient.disconnect();
+        }
+    }
+	
+	public void initMap() {
+        initLocationClient();
+        mLocationClient.connect();
+    	// Do a null check to confirm that we have not already instantiated the map.
+        if (theMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            theMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (theMap != null) {
+            	theMap.setMyLocationEnabled(true);
+            	theMap.setOnMyLocationButtonClickListener(this);
+                setUpMap();
+            }
+        }
+    }
+    
+    private void initLocationClient() {
+        if (mLocationClient == null) {
+            mLocationClient = new LocationClient( 
+            		getApplicationContext(),
+            		this,  // ConnectionCallbacks
+            		this); // OnConnectionFailedListener
+        }
+    }
+    
+    /**
+     * Callback called when connected to GCore. Implementation of {@link ConnectionCallbacks}.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLocationClient.requestLocationUpdates(
+                REQUEST,
+                this);  // LocationListener
+        centerMap();
+    }
+
+    /**
+     * Callback called when disconnected from GCore. Implementation of {@link ConnectionCallbacks}.
+     */
+    @Override
+    public void onDisconnected() {
+        // Do nothing
+    }
+
+    /**
+     * Implementation of {@link OnConnectionFailedListener}.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Do nothing
+    }
+    
+    private void setUpMap()
+    {
+    	centerMap();
+    	//mBurien = theMap.addMarker(new MarkerOptions()
+    			//.position(BURIEN)
+                //.title("Burien")
+                //.snippet("Population: 49,410")
+                //.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+    }
+    
+    private void centerMap()
+    {
+    	if (mLocationClient!=null)
+    	{
+	    	Location loc = mLocationClient.getLastLocation();
+	    	LatLng latlng = new LatLng(loc.getLatitude(), loc.getLongitude());
+	    	theMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 16));
+    	}
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
